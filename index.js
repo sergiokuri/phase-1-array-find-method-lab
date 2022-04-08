c37ded0091b74f31792287112c21392091eff63f
Sergio <kurisergio777777@gmail.com>
2022-04-07 20:18:16 -0400

    commit tree 5fe5e9684340380372967f6253f4895adde3c172

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // code your solution here
+
+
+
+function superbowlWin (record){
+    let check = record.find( item => item.result === "W")
+        if (!!check){
+            return check.year
+
+        }else{
+            return undefined
+    }
+}
+
+
+
+
+
+/*
+function superbowWin(){
+    if (record[0].result === "W"){
+        return record
+    }else if(record[1].result === "W") {
+        console.log(`year ${record[1].year}`)
+    }else if (record[2].result === "W"){
+        console.log(`year ${record[2].year}`)   
+    }else {
+        console.log("no one wins")
+    } 
+    
+}
+superbowWin();
+
+
+
+*/
